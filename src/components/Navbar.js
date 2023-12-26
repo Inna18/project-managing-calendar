ecbac91cbcd7ac53e9d19fbe398318037b64cb22
Inna18 <kinvdev21@gmail.com>
2023-12-26 10:22:06 +0900

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import "./Navbar.css"
+import Temple from "../assets/temple.svg"
+import {Link} from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div  className="navbar">
+      <ul>
+        <li className="logo">
+          <img src={Temple} alt="logo img"/>
+          <span>Inna's Console</span>
+        </li>
+
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+        <li><button>Logout</button></li>
+      </ul>
+    </div>
+  )
+}
